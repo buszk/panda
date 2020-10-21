fcebc0f56ff0a982f6c1888c4fbeec37a8295bc5
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-10-21 00:53:35 +0000

    add: readl addresses

--- include/llvm-pc-filter.h
+++ include/llvm-pc-filter.h
@@ -1,5 +1,13 @@
 #include "inttypes.h"
+
+#define in_range(x, l, r) \
+    (x >= l && x< r)
+#define in_block(pc, target) \
+    in_range(pc, target-0x100, target+0x100)
+
 static int llvm_translate_pc(uint64_t pc) {
     return pc >= 0xffffffffa0000000 ||
-           (pc >= 0xffffffff81c62000 && pc < 0xffffffff81c63000);
+           in_block(pc, 0xffffffff81c62479) ||
+           in_block(pc, 0xffffffff810c7136) ||
+           in_block(pc, 0xffffffff810ca047);
 }
