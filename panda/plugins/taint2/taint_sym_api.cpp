79902c1808fff3f0992e7b0f13fe8a23eb10714d
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-11-04 22:34:47 +0000

    only output flippable branch

--- panda/plugins/taint2/taint_sym_api.cpp
+++ panda/plugins/taint2/taint_sym_api.cpp
@@ -12,8 +12,10 @@
 
 #include <fstream>
 #include <sstream>
+#include <iostream>
 z3::context context;
 std::vector<z3::expr> path_constraints;
+bool z3_failure = false;
 
 void taint2_sym_label_addr(Addr a, int offset, uint32_t l) {
     assert(shadow);
@@ -48,6 +50,9 @@ void reg_branch_pc(z3::expr condition, bool concrete) {
 
     static bool first = true;
     static int count = 0;
+
+    if (z3_failure) return;
+
     count ++;
     target_ulong current_pc = panda_current_pc(first_cpu);
 
@@ -56,11 +61,42 @@ void reg_branch_pc(z3::expr condition, bool concrete) {
     z3::solver solver(context);
     if (pc.is_true() || pc.is_false())
         return;
+
+    for (auto c: path_constraints) {
+        solver.add(c);
+    }
+
+    // If this fail, z3 cannot solve current path
+    // Possible reasons:
+    //     Code not instrumented
+    //     Unimplemented instructions
+    switch (solver.check()) {
+        case z3::check_result::unsat:
+            std::cerr << "Error: Z3 find current path UNSAT "
+                << std::hex << current_pc << std::dec << std::endl;
+            z3_failure= true;
+            return;
+        case z3::check_result::unknown:
+            std::cerr << "Warning: Z3 cannot sovle current path "
+                << std::hex << current_pc << std::dec << std::endl;
+            z3_failure= true;
+            return;
+        default:
+            break;
+    }
+
+    solver.add(!pc);
+    path_constraints.push_back(pc);
+
+    // If this fail, current branch cannot be reverted
+    if (solver.check() != z3::check_result::sat)
+        return;
+
     std::ofstream ofs("/tmp/drifuzz_path_constraints", 
             first ? std::ofstream::out : std::ofstream::app);
     
     first = false;
-    
+
     ofs << "========== Z3 Path Solver ==========\n";
     
     ofs << "Count: " << count << " Condition: " << concrete <<
@@ -68,29 +104,11 @@ void reg_branch_pc(z3::expr condition, bool concrete) {
 
     ofs << "Path constraint: \n" << pc << "\n";
 
-    for (auto c: path_constraints) {
-        solver.add(c);
-    }
-    if (unlikely(solver.check() != z3::check_result::sat)) {
-        for (auto c: path_constraints) {
-            ofs << c << "\n";
-        }
-        ofs << "========== Z3 Path Solver End ==========\n";
-        ofs.close();
-        return;
-    }
-    path_constraints.push_back(pc);
-    // If this fail, current path diverge, z3 cannot solve
-    assert (solver.check() == z3::check_result::sat);
-
-    solver.add(!pc);
-    if (solver.check() == z3::check_result::sat) {
-        z3::model model(solver.get_model());
-        for (int i = 0; i < model.num_consts(); i++) {
-            z3::func_decl f = model.get_const_decl(i);
-            z3::expr pc_not = model.get_const_interp(f);
-            ofs << "Inverted value: " << f.name().str() << " = " << pc_not <<  "\n";
-        }
+    z3::model model(solver.get_model());
+    for (int i = 0; i < model.num_consts(); i++) {
+        z3::func_decl f = model.get_const_decl(i);
+        z3::expr pc_not = model.get_const_interp(f);
+        ofs << "Inverted value: " << f.name().str() << " = " << pc_not <<  "\n";
     }
     ofs << "========== Z3 Path Solver End ==========\n";
     ofs.close();
