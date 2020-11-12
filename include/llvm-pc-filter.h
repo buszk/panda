28eb8d10f0a2a2b0892ba7bc13d71aedf942b528
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-11-12 03:58:42 +0000

    detailed tracked functions

--- include/llvm-pc-filter.h
+++ include/llvm-pc-filter.h
@@ -2,12 +2,11 @@
 
 #define in_range(x, l, r) \
     (x >= l && x<= r)
-#define in_block(pc, target) \
-    in_range(pc, target-0x100, target+0x100)
 
 static int llvm_translate_pc(uint64_t pc) {
     return pc >= 0xffffffffa0000000 ||
-           in_block(pc, 0xffffffff81c62479) || //readl
+           in_range(pc, 0xffffffff81c62410, 0xffffffff81c62443) || //ioread16
+           in_range(pc, 0xffffffff81c62450, 0xffffffff81c6247b) || //ioread32
            in_range(pc, 0xffffffff8346bc50, 0xffffffff8346bc69) || //__memcpy
            in_range(pc, 0xffffffff8346bc70, 0xffffffff8346bc78) || //memcpy_erms
            in_range(pc, 0xffffffff8346bc80, 0xffffffff8346bd8e) || //memcpy_orig
