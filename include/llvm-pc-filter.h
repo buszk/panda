724cae582ef58a8cfeb4183dbda489b5c8729e11
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-03-12 04:36:11 +0000

    update

--- include/llvm-pc-filter.h
+++ include/llvm-pc-filter.h
@@ -8,9 +8,9 @@ static int llvm_translate_pc(uint64_t pc) {
            in_range(pc, 0xffffffff81c623d0, 0xffffffff81c62401) || //ioread8
            in_range(pc, 0xffffffff81c62410, 0xffffffff81c62443) || //ioread16
            in_range(pc, 0xffffffff81c62450, 0xffffffff81c6247b) || //ioread32
-           in_range(pc, 0xffffffff834904f0, 0xffffffff83490509) || //__memcpy
-           in_range(pc, 0xffffffff83490510, 0xffffffff83490518) || //memcpy_erms
-           in_range(pc, 0xffffffff83490520, 0xffffffff8349062e) || //memcpy_orig
+           in_range(pc, 0xffffffff83486500, 0xffffffff83486519) || //__memcpy
+           in_range(pc, 0xffffffff83486520, 0xffffffff83486528) || //memcpy_erms
+           in_range(pc, 0xffffffff83486530, 0xffffffff8348663e) || //memcpy_orig
            in_range(pc, 0xffffffff83600920, 0xffffffff836009df) || //interrupt_entry
            in_range(pc, 0xffffffff83600aaf, 0xffffffff83600acc) || //restore_regs_and_return_to_kernel
            in_range(pc, 0xffffffff836009e0, 0xffffffff836009fc) || //common_spurious
